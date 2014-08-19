cb9dd772455b26068ca64477debca42c431fcc0e
peterxiemin <peterxiemin@163.com>
2014-08-19 17:59:05 +0800

    modified:   common/icl_sig.c

--- common/icl_sig.c
+++ common/icl_sig.c
@@ -16,33 +16,37 @@
 #define SIGCOUNT 64
 
 int sig[SIGCOUNT];
-static int index;
 
 void icl_sig_handler(int sig)
 {
-    printf("icl_sig_handler %d\n", sig);
+	/* 注意，事件处理函数不能使用可重用函数 printf malloc 等
+	 * 最好设置flag， 由函数外判断  */
+	;;
 }
 
 int icl_sig_register(int index) 
 {
-	if (sig[index] == 0) {
-		sig[index] = 1;
+	if (index > 0 && index <= 64) {
+		if (sig[index] == 0) {
+			sig[index] = index;
+		}
+		else {
+			printf("sig %d already register!\n", index);
+		}
 	}
-	else {
-		printf("sig %d already register!\n", index);
-	}
+
 	return 0;
 }
 
 void icl_sig_setup(void)
 {
-    int i;
-    struct sigaction action;
-    sigemptyset(&action.sa_mask);
+	int i;
+	struct sigaction action;
+	sigemptyset(&action.sa_mask);
 	action.sa_flags = 0;
 	action.sa_handler = icl_sig_handler;
 	for (i = 0; i < SIGCOUNT; i++ ) {
-		if (sig[i] == 1 && sigaction(sig[i], &action, (struct sigaction*)0) < 0)
+		if (sig[i] != 0 && sigaction(sig[i], &action, (struct sigaction*)0) < 0)
 		{
 			printf("sigaction: %s\n fital.", sig[i]);
 		}

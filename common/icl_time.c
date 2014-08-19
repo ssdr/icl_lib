1571df4b0cc1979e657830b36e3698ae84bd2180
peterxiemin <peterxiemin@163.com>
2014-08-19 14:39:15 +0800

    time

--- common/icl_time.c
+++ common/icl_time.c
@@ -9,19 +9,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 
 char *icl_asctime()
 {
-typedef struct tm STRC_TM;
-    char *time_str;
-    int pos;
-    time_t t_clock;
-    STRC_TM *stm;
-    (void) time(&t_clock);
-    stm = localtime(&t_clock);
-    time_str = asctime(stm);
-    pos = strlen(time_str);
-    //删除最后最后一个换行符
-    time_str[pos-1] = '\0';
-    return time_str;
+	typedef struct tm STRC_TM;
+	char *time_str;
+	int pos;
+	time_t t_clock;
+	STRC_TM *stm;
+	(void) time(&t_clock);
+	stm = localtime(&t_clock);
+	time_str = asctime(stm);
+	pos = strlen(time_str);
+	//删除最后最后一个换行符
+	time_str[pos-1] = '\0';
+	return time_str;
+}
+
+void icl_prtnow()
+{
+	char pTime[50];
+	int		temI, t_Loop, t_Len;
+	char	t_Room[128] = { 0x00  } ;
+	time_t	t_When;
+	struct timeval  t_Pri_Time;
+	struct timezone tz;
+	gettimeofday(&t_Pri_Time , &tz);
+	time( &t_When  );
+	sprintf( pTime, "%s", ctime( &t_When  )  );
+	t_Loop = strlen( pTime  );
+	for( temI=0; temI<t_Loop; temI++  )
+	{
+		if ( *( pTime + temI  ) == 0x0D ||
+				*( pTime + temI  ) == 0x0A
+		   )
+		{
+			*( pTime + temI  ) = 0x00;
+			t_Len = strlen( pTime  );
+			sprintf( t_Room, " ( %ld.%.3ld  )", t_Pri_Time.tv_sec, t_Pri_Time.tv_usec / 1000  );
+		}
+	}
+	printf("%s, %s\n", pTime, t_Room);
 }

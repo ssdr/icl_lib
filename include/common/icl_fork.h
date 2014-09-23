37d09224a8be3a7bd90fe2e6db4c07a5f8179f97
peterxiemin <peterxiemin@163.com>
2014-09-23 09:33:50 +0800

    new file:   include/common/icl_fork.h 	deleted:    common/icl_master.c 	deleted:    include/common/icl_master.h 	deleted:    include/icl_fork.h

---
+++ include/common/icl_fork.h
@@ -0,0 +1,19 @@
+/*
+ * icl_fork.h
+ *
+ *  Created on: 2014年9月22日
+ *      Author: peterxmw
+ */
+
+
+
+#ifndef ICL_FORK_H_
+#define ICL_FORK_H_
+
+#include <stdio.h>
+
+char *icl_getenv(const char *name);
+int icl_putenv(const char *str);
+int icl_setenv(const char *name, const char *value, int rewrite);
+
+#endif /*ICL_FORK_H_*/

59b37e983b46a80d248926e7d63da4db2feb6f3f
Damian <ssdr_liu@163.com>
2014-09-30 15:02:15 +0800

    new file:   include/net/icl_net_epoll.h 	new file:   include/net/icl_net_select.h 	new file:   net/icl_net_epoll.c 	new file:   net/icl_net_select.c 	deleted:    include/net/icl_wrapi.h 	deleted:    net/icl_wrapi.c

---
+++ include/net/icl_net_select.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * author: xiemin
+ * filename: icl_net_select.h
+ * company: ifeng
+ * date: 2014-09-30
+ *
+ *
+ */
+
+
+#ifndef ICL_NET_SELECT_H
+#define ICL_NET_SELECT_H
+
+#endif /*ICL_NET_SELECT_H*/
+
+
+
---
+++ net/icl_net_select.c
@@ -0,0 +1,11 @@
+/*
+ *
+ * author: xiemin
+ * filename: icl_net_select.c
+ * company: ifeng
+ * date: 2014-09-30
+ *
+ *
+ */
+
+
